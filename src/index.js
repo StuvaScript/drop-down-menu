6df1ce8ef8c7a8166065222987fc9bd67fde1b8d
Stu <stumac1000@yahoo.com>
2023-10-14 12:06:54 -0700

    Start on moving everything in JS from CSS

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import './normalize.css';
 import './style.css';
+
+const backgroundColor = '#1ce';
+
+const target = document.querySelectorAll('.drop-down-target');
+console.log(target);
+const box = document.querySelector('.drop-down-box');
+
+[...target].map((obj) => {
+  Object.assign(obj.style, {
+    background: backgroundColor,
+    color: 'white',
+    fontSize: '4rem',
+  });
+});
+
+//todo **`` Going to change the above code to style the 'box' instead of the 'target'. Then the ':hover' event is going to be replicated using either 'mouseover' or 'mouseenter' event listeners.
